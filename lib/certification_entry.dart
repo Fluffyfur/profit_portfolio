78c97e7f09a6620510ec8a34a770eb0e42c97a58
Fluffyfur <138587704+Fluffyfur@users.noreply.github.com>
2023-12-08 09:26:34 +0900

    20231208_work

--- lib/certification_entry.dart
+++ lib/certification_entry.dart
@@ -24,126 +24,151 @@ class _CertificationEntryPageState extends State<CertificationEntryPage> {
       List<Certification> certifications = infoservice.certifications;
 
       return Scaffold(
-          appBar: AppBar(
-            title: Text('Add Certification'),
-          ),
-          body: certifications.isEmpty
-              ? Row(
-                  children: [
-                    Flexible(
-                      child: TextField(
-                        controller: certificationNameController,
-                        decoration: InputDecoration(
-                          labelText: '자격증명',
-                          labelStyle: TextStyle(
-                            color: Colors.grey,
-                          ),
+        appBar: AppBar(
+          title: Text('Add Certification'),
+        ),
+        body: certifications.isEmpty
+            ? Row(
+                children: [
+                  Flexible(
+                    child: TextField(
+                      controller: certificationNameController,
+                      decoration: InputDecoration(
+                        labelText: '자격증명',
+                        labelStyle: TextStyle(
+                          color: Colors.grey,
                         ),
                       ),
                     ),
-                    Flexible(
-                      child: TextField(
-                        controller: acquisitionDateController,
-                        decoration: InputDecoration(
-                          labelText: '취득일자',
-                          labelStyle: TextStyle(
-                            color: Colors.grey,
-                          ),
+                  ),
+                  Flexible(
+                    child: TextField(
+                      controller: acquisitionDateController,
+                      decoration: InputDecoration(
+                        labelText: '취득일자',
+                        labelStyle: TextStyle(
+                          color: Colors.grey,
                         ),
                       ),
                     ),
-                    Flexible(
-                      child: TextField(
-                        controller: organizerController,
-                        decoration: InputDecoration(
-                          labelText: '취득처',
-                          labelStyle: TextStyle(
-                            color: Colors.grey,
-                          ),
+                  ),
+                  Flexible(
+                    child: TextField(
+                      controller: organizerController,
+                      decoration: InputDecoration(
+                        labelText: '취득처',
+                        labelStyle: TextStyle(
+                          color: Colors.grey,
                         ),
                       ),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Certification certification = Certification(
-                          certificationName: certificationNameController.text,
-                          acquisitionDate: acquisitionDateController.text,
-                          organizer: organizerController.text,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Certification certification = Certification(
+                        certificationName: certificationNameController.text,
+                        acquisitionDate: acquisitionDateController.text,
+                        organizer: organizerController.text,
+                      );
+                      Provider.of<InfoService>(context, listen: false)
+                          .addCertification(certification);
+
+                      certificationNameController.clear();
+                      acquisitionDateController.clear();
+                      organizerController.clear();
+                    },
+                    child: Text('추가'),
+                  ),
+                ],
+              )
+            : Column(
+                children: [
+                  Container(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: certifications.length,
+                      itemBuilder: (context, index) {
+                        Certification cert = certifications[index];
+
+                        return Container(
+                          width: double.infinity,
+                          child: Row(
+                            children: [
+                              Text(
+                                cert.certificationName,
+                              ),
+                              Spacer(),
+                              Text(cert.acquisitionDate),
+                              Spacer(),
+                              Text(cert.organizer),
+                              Spacer(),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text('삭제'),
+                              ),
+                            ],
+                          ),
                         );
-                        Provider.of<InfoService>(context, listen: false)
-                            .addCertification(certification);
                       },
-                      child: Text('추가'),
                     ),
-                  ],
-                )
-              : ListView.builder(
-                  itemCount: certifications.length,
-                  itemBuilder: (context, index) {
-                    Certification cert = certifications[index];
+                  ),
+                  Row(
+                    children: [
+                      Flexible(
+                        child: TextField(
+                          controller: certificationNameController,
+                          decoration: InputDecoration(
+                            labelText: '자격증명',
+                            labelStyle: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: TextField(
+                          controller: acquisitionDateController,
+                          decoration: InputDecoration(
+                            labelText: '취득일자',
+                            labelStyle: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: TextField(
+                          controller: organizerController,
+                          decoration: InputDecoration(
+                            labelText: '취득처',
+                            labelStyle: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Certification certification = Certification(
+                            certificationName: certificationNameController.text,
+                            acquisitionDate: acquisitionDateController.text,
+                            organizer: organizerController.text,
+                          );
+                          Provider.of<InfoService>(context, listen: false)
+                              .addCertification(certification);
 
-                    return Row(
-                      children: [
-                        Flexible(
-                          child: TextField(
-                            controller: certificationNameController,
-                            decoration: InputDecoration(
-                              labelText: cert.certificationName,
-                              labelStyle: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Flexible(
-                          child: TextField(
-                            controller: acquisitionDateController,
-                            decoration: InputDecoration(
-                              labelText: cert.acquisitionDate,
-                              labelStyle: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Flexible(
-                          child: TextField(
-                            controller: organizerController,
-                            decoration: InputDecoration(
-                              labelText: cert.organizer,
-                              labelStyle: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            Certification certification = Certification(
-                              certificationName:
-                                  certificationNameController.text,
-                              acquisitionDate: acquisitionDateController.text,
-                              organizer: organizerController.text,
-                            );
-                            Provider.of<InfoService>(context, listen: false)
-                                .addCertification(certification);
-
-                            Navigator.pop(
-                              context,
-                              Certification(
-                                certificationName:
-                                    certificationNameController.text,
-                                acquisitionDate: acquisitionDateController.text,
-                                organizer: organizerController.text,
-                              ),
-                            );
-                          },
-                          child: Text('추가'),
-                        ),
-                      ],
-                    );
-                  },
-                ));
+                          certificationNameController.clear();
+                          acquisitionDateController.clear();
+                          organizerController.clear();
+                        },
+                        child: Text('추가'),
+                      ),
+                    ],
+                  ),
+                  TextButton(onPressed: () {}, child: Text("저장"))
+                ],
+              ),
+      );
     });
   }
 }
